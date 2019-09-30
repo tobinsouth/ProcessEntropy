060c799ba55a513f6005cabde92e726853676dfe
tmsouth <tobin.south@gmail.com>
2019-10-01 00:00:56 +0930

    Removing scripts from setup

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name='ProcessEntropy',
     version='0.2dev',
     packages=['ProcessEntropy',],
-    scripts=['example_usage', 'test_code'] ,
     license='MIT license',
     description='A toolkit for calculating process entropy quickly. With specific applications to tweets.',
     long_description=open('README.md').read(),
