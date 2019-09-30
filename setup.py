888c95aef7bbeb20b9a5a64b6f2f61487b9a8bf4
tmsouth <tobin.south@gmail.com>
2019-09-30 23:57:59 +0930

    Added test scripts to setup

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     name='ProcessEntropy',
     version='0.2dev',
     packages=['ProcessEntropy',],
+    scripts=['example_usage', 'test_code'] ,
     license='MIT license',
     description='A toolkit for calculating process entropy quickly. With specific applications to tweets.',
     long_description=open('README.md').read(),
+    url="https://github.com/tobinsouth/ProcessEntropy",
     install_requires=[
         "numba",
         "nltk",
