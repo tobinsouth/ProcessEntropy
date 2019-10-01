a85fe8133bcfe084ecfea58cc4a2ee26f78bb255
tmsouth <tobin.south@gmail.com>
2019-10-01 14:55:28 +0930

    Update Version for PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='ProcessEntropy',
-    version='0.2',
+    version='0.3',
     packages=['ProcessEntropy',],
     license='MIT license',
     author='Tobin South',
