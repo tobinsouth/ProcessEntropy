2d16c6a23c1ac902b59f474ca82b7b9e442330a7
bridget-smart <bridget.anna.smart@gmail.com>
2023-03-24 11:57:58 +1030

    Typed output of hash to ensure C compatibility

--- ProcessEntropy/Preprocessing.py
+++ ProcessEntropy/Preprocessing.py
@@ -33,7 +33,7 @@ def fnv(data):
     for byte in data:
         hval = (hval * 0x100000001b3) % 2**32
         hval = hval ^ byte
-    return hval
+    return np.uint32(hval)
 
 
 def custom_tokenize(text, basic_tokenize = False):
@@ -87,6 +87,7 @@ def tweet_to_hash_array(text):
             except UnicodeEncodeError:
                 continue # They're simply ignored
         return encoded
+
 
 
 
